ebe60b20e60624389cd0c939c1d958ce3a303f5a
unknown <spriyam1221@gamil.com>
2022-03-10 12:01:28 +0530

    added addition  features

---
+++ src/main/java/com/priya/Addition.java
@@ -0,0 +1,21 @@
+package com.priya;
+
+public class Addition {
+
+public int firstnumber;
+public int secondnumber;
+
+	public Addition(int a, int b)
+	{
+		this.firstnumber=a;
+		this.secondnumber=b;
+		
+	}	
+	public int add()
+	{
+		
+		return this.firstnumber+this.secondnumber;
+	}
+				
+	
+}
---
+++ src/test/java/com/priya/AdditionTest.java
@@ -0,0 +1,10 @@
+package com.priya;
+
+public class AdditionTest {
+	public static void main(String[] args) {
+		Addition obj1=new Addition(10,20);
+		int result=obj1.add();
+		System.out.println(result);
+	}
+
+}
